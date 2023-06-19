c303ec60f8706ba8e8476015a23943cb713f2e79
gedsouza <emillyn_muniz@hotmail.com>
2023-06-18 22:44:55 -0300

    utilizando terraform com ansible

--- main.tf
+++ main.tf
@@ -17,8 +17,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0430580de6244e02e"
   instance_type = "t2.micro"
   key_name = "iac-alura"
-
+  # user_data = <<-EOF
+  #               #!/bin/bash
+  #               cd /home/ubuntu
+  #               echo "<h1>Feito com Terraform 2</h1>" > index.html
+  #               nohup busybox httpd -f -p 8080 &
+  #               EOF
   tags = {
-    Name = "Primeira Instancia"
+    Name = "Terraform Ansible Python"
   }
 }
